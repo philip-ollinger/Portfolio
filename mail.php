9b14cb4c02f08c4b083d25b5679e8e7e472afd9a
Donald Duck <konten.info@gmx.at>
2021-06-29 17:47:30 +0200

    Error fixed

--- mail.php
+++ mail.php
@@ -7,5 +7,4 @@ $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
-header( "refresh:2;url=https://portfolio.philip-ollinger.at" );
 ?>
