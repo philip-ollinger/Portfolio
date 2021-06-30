d9a1b65887be0b0fdb54246dc108bbd1d88b00ba
Donald Duck <konten.info@gmx.at>
2021-06-30 16:59:32 +0200

    Mail php security feature test added

--- mail.php
+++ mail.php
@@ -10,3 +10,30 @@ echo "Thank You!";
 ?>
 
 <meta http-equiv="refresh" content="2;url=https://portfolio.philip-ollinger.at"> 
+
+<?php
+
+$errors = [];
+
+if (!empty($_POST)) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+
+   if (empty($name)) {
+       $errors[] = 'Name is empty';
+   }
+
+   if (empty($email)) {
+       $errors[] = 'Email is empty';
+   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $errors[] = 'Email is invalid';
+   }
+
+   if (empty($message)) {
+       $errors[] = 'Message is empty';
+   }
+}
+?>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
